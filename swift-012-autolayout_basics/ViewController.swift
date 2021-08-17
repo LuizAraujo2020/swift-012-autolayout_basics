9ecea7cd3d1cf8bd33a26e22cf6459e3bf5f959e
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-16 21:09:12 -0300

    Initial Commit

---
+++ swift-012-autolayout_basics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-012-autolayout_basics
+//
+//  Created by Luiz Carlos da Silva Araujo on 16/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
